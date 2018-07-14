db5af3900b3db0fd79869ae69cdc7db37c75e6a9
Joana H <joana_hrotko@hotmail.com>
2018-07-14 14:25:42 +0100

    Simple Bank class
    #3

---
+++ bank.py
@@ -0,0 +1,16 @@
+from account import Account
+
+
+class Bank:
+
+    def __init__(self):
+        self.total_account = 0
+        self.accounts = list()
+
+    def add_account(self, account):
+        self.accounts.append(account)
+        self.total_account += 1
+
+    def create_account(self, amount):
+        acount = Account(amount, self.total_account)
+        self.accounts.append(acount)
